e02435d623d572a4f82d245ae659cee1b06b1ca4
Sandro Panighetti <sandro.panighetti@gmail.com>
2018-09-15 08:52:24 +0200

    added mysql

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var mysql = require('mysql');
+
+
+// db stuff
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "sandro",
+  password: "sandrosandro"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.get('/listMeals', function (req, res) {
    fs.readFile( __dirname + "/" + "meals.json", 'utf8', function (err, data) {
        console.log( data );
        res.end( data );
    });
+})
+
+app.get('/listRecipes', function (req, res){
+
 })
 
 var server = app.listen(8081, function () {
